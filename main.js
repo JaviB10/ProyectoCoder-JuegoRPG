386302fc40dc3d63fc58b68ed5fad0a050821cf7
JaviB10 <javi41@gmail.com>
2022-09-26 17:57:08 -0300

    Correccion de datos de los heroes

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ let heroe = () => {
             document.write ("<p> Descripcion: " + heroe.descripcion + "</p>")
             break;
         case "2":
-            heroe = new Personaje ("Guerrero", 1000, 60, "Clase de personaje cuya mayor habilidad o principal función es el ataque a corta distancia o cuerpo a cuerpo.");
+            heroe = new Personaje ("Mago", 800, 80, "Clase de personaje con una defensa debil pero cuya habilidad magica le da un ataque superior al resto.");
             document.write ("<p> ELEGISTE AL SIGUIENTE PERSONAJE </p>")
             document.write ("<p> Clase: " + heroe.clase + "</p>")
             document.write ("<p> Vida: " + heroe.vida + "</p>")
@@ -37,7 +37,7 @@ let heroe = () => {
             document.write ("<p> Descripcion: " + heroe.descripcion + "</p>") 
             break;
         case "3":
-            heroe = new Personaje ("Guerrero", 1000, 60, "Clase de personaje cuya mayor habilidad o principal función es el ataque a corta distancia o cuerpo a cuerpo.");
+            heroe = new Personaje ("Arquero", 900, 50, "Clase de personaje que efectua ataques a distancia con poco poder pero cuenta con una defensa aceptable.");
             document.write ("<p> ELEGISTE AL SIGUIENTE PERSONAJE </p>")
             document.write ("<p> Clase: " + heroe.clase + "</p>")
             document.write ("<p> Vida: " + heroe.vida + "</p>")
